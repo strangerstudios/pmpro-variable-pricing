9fcbd44e75e39dc16fdedfa3c796ea187490fe3f
Jason Coleman <jason@strangerstudios.com>
2017-12-08 19:39:52 -0500

    Fixed some bugs in JS and made compatible with other addons.

--- javascript/pmpro-variable-pricing.js
+++ javascript/pmpro-variable-pricing.js
@@ -18,89 +18,108 @@
  *
  */
 
-
 //some vars for keeping track of whether or not we show billing
 var pmpro_gateway_billing;
 var pmpro_pricing_billing;
 
+//this var is used by several pmpro add ons to keep track of whether or not the billing fields are required
 if ( pmpro_require_billing == null ) {
     var pmpro_require_billing = true;
 }
+
+//keep track of our obect
+var PMProVariablePricing;
+
 //this script will hide show billing fields based on the price set
 jQuery(document).ready(function() {
     "use strict";
-    var variablePricing = {
+    PMProVariablePricing = {
         init: function() {
-           
-			pmpro_gateway_billing = pmprovp.settings.gateway_billing;
-            pmpro_pricing_billing = pmprovp.settings.pricing_billing;            
 
-            var self = this;
+        	//setup these global vars
+        	pmpro_gateway_billing = pmprovp.settings.gateway_billing;
+            pmpro_pricing_billing = pmprovp.settings.pricing_billing;
 
-            // Exit if there are payment choices available
-            if ( jQuery('#pmpro_payment_method').length > 0 ) {
-                return;
-            }
+        	//some vars           
+            this.no_billing_gateways = ['paypalstandard', 'paypalexpress', 'twocheckout', 'check'];
+            this.priceElem = jQuery('#price');
+            this.addressFields = jQuery('#pmpro_billing_address_fields');
+            this.paymentInfo = jQuery('#pmpro_payment_information_fields');
+            this.paymentMethod = jQuery('#pmpro_payment_method');
+            this.regSubmitSpan = jQuery('#pmpro_submit_span');
+			this.ppeSubmitSpan = jQuery('#pmpro_paypalexpress_checkout');
 			
             //bind check to price field
 			this.price_timer = null;
-            jQuery('#price').bind('keyup change', function() {                
-				self.price_timer = setTimeout(self.checkForFree, 500);
+            this.priceElem.bind('keyup change', function() {                
+				//use our global var name here, since we're in a closure
+				PMProVariablePricing.price_timer = setTimeout(PMProVariablePricing.checkForFree, 500);
             });            
 
 			//get gateway value and bind check to gateway field
-            if(jQuery('input[name=gateway]').length) {
-				self.gateway = jQuery('input[name=gateway]').is(':checked').val();
+            if(typeof this.paymentMethod == 'object') {
+				this.gateway = jQuery('input[name=gateway]:checked').val();
 				jQuery('input[name=gateway]').bind('click', function() {
-                    self.price_timer = setTimeout(self.checkForFree, 500);
+                    //use our global var name here, since we're in a closure
+                    PMProVariablePricing.price_timer = setTimeout(PMProVariablePricing.checkForFree, 500);
                 });
 			} else {
-				self.gateway = pmprovp.settings.gateway;	
+				this.gateway = pmprovp.settings.gateway;	
 			}
 			
             //check when page loads too
-            self.checkForFree();
+            this.checkForFree();
         },
         checkForFree: function() {
-
-            var self = this;
-            var priceElem = jQuery('#price');
-            var addressFields = jQuery('#pmpro_billing_address_fields');
-            var paymentInfo = jQuery('#pmpro_payment_information_fields');
-
-            var price = parseFloat(priceElem.val());
-
-            //does the gateway require billing?            
-			if(jQuery('input[name=gateway]').length) {            
-                var no_billing_gateways = ['paypalexpress', 'twocheckout'];
-                self.gateway = jQuery('input[name=gateway]').is(':checked').val();
-
-                if(no_billing_gateways.indexOf(self.gateway) > -1) {
-                    pmpro_gateway_billing = false;
-                } else {
-                    pmpro_gateway_billing = true;
-                }
-            }
-						
-            //is there a price?
+        	
+        	//get the current price
+            var price = parseFloat(PMProVariablePricing.priceElem.val());
             if(price > 0) {
                 pmpro_pricing_billing = true;
             } else {
                 pmpro_pricing_billing = false;
             }
-						
+
+            //if there is a payment method radio, get the current gateway
+			if(typeof PMProVariablePricing.paymentMethod == 'object') {
+                PMProVariablePricing.gateway = jQuery('input[name=gateway]:checked').val();
+
+                //some gateways require billing fields, others don't
+                if(PMProVariablePricing.no_billing_gateways.indexOf(PMProVariablePricing.gateway) > -1) {
+                    pmpro_gateway_billing = false;
+                } else {
+                    pmpro_gateway_billing = true;
+                }
+            }
+
             //figure out if we should show the billing fields
-            if(pmpro_gateway_billing && pmpro_pricing_billing) {                
-				addressFields.show();
-                paymentInfo.show();
+            if(pmpro_gateway_billing && pmpro_pricing_billing) {   
+				PMProVariablePricing.addressFields.show();
+                PMProVariablePricing.paymentInfo.show();
                 pmpro_require_billing = true;
             } else {
-                addressFields.hide();
-                paymentInfo.hide();
+                PMProVariablePricing.addressFields.hide();
+                PMProVariablePricing.paymentInfo.hide();
+                PMProVariablePricing.paymentMethod.hide();
                 pmpro_require_billing = false;
+            }
+
+            //toggle the payment method box if available and which type of checkout button shown
+            if(pmpro_pricing_billing) {
+            	PMProVariablePricing.paymentMethod.show();                
+                if(PMProVariablePricing.gateway == 'paypalexpress' || PMProVariablePricing.gateway == 'paypalstandard') {
+                	PMProVariablePricing.regSubmitSpan.hide();
+                	PMProVariablePricing.ppeSubmitSpan.show();
+                } else {
+                	PMProVariablePricing.ppeSubmitSpan.hide();
+                	PMProVariablePricing.regSubmitSpan.show();
+                }
+            } else {
+            	PMProVariablePricing.ppeSubmitSpan.hide();
+                PMProVariablePricing.regSubmitSpan.show();
             }
         }
     }
 	
-    variablePricing.init();
+    PMProVariablePricing.init();
 });
